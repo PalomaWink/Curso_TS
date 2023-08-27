24825df9b534ab2bd734963a6b4dc94a049e4868
Paloma Wink <paloma.wink@hotmail.com>
2023-08-27 11:38:53 -0300

    Finalizando seção 2

--- 2_Fundamentos/index.ts
+++ 2_Fundamentos/index.ts
@@ -7,3 +7,14 @@ x = 20.545421 // Float tambem e considerado number
 
 x.toPrecision(2) // Quando eu tipo, so consigo usar e acessar o que é atribuido aquele tipo, ou seja, aqui eu consigo usar um .toFixed tambem, mas
 // não consigo usar um .toUpperCase(), por que isso so funciona com string
+
+// tsc -w compila automaticamente
+
+// Desafio 2
+const a: number = 1;
+const numberString = a.toString();
+
+const printNumber: string = `EU vou imprimir o número ${numberString}`
+
+console.log(printNumber);
+
