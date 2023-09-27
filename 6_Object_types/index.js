fd0e49666ce027898bc05ba406a65a90df694824
Paloma Wink <paloma.wink@hotmail.com>
2023-09-27 16:02:01 -0300

    trabalhando com propriedade opcional, readonly e index signature

--- 6_Object_types/index.js
+++ 6_Object_types/index.js
@@ -12,3 +12,29 @@ const shirt = {
     isAvailable: true
 };
 showProductDetails(shirt);
+function showUserDetails(user) {
+    console.log(`O usu[ario tem o e-mail ${user.email}`);
+    if (user.role) {
+        console.log(`A funcao do usuario e: ${user.role}`); // Se nao faz essa validacao aparece na tela undefined para o usuario, por ser um valor opcional
+    }
+}
+const u1 = {
+    email: 'teste@teste.com',
+    role: 'Admin'
+};
+const u2 = {
+    email: 'teste@teste.com',
+};
+showUserDetails(u1);
+showUserDetails(u2);
+const fusca = {
+    brand: 'VW',
+    wheels: 4
+};
+console.log(fusca);
+// fusca.wheels = 5 -> aparce o seguinte erro 'Não é possível atribuir a 'wheels' porque é uma propriedade de somente leitura.'
+fusca.brand = 'Chevrolet'; // aceita de boas
+let coords = {
+    x: 10
+};
+coords.y = 15;
--- 6_Object_types/index.ts
+++ 6_Object_types/index.ts
@@ -20,3 +20,58 @@ const shirt: Product = {
 }
 
 showProductDetails(shirt)
+
+//2 - Propriedade opcional em interface
+
+interface User {
+  email: string,
+  role?: string
+}
+
+function showUserDetails(user: User) {
+  console.log(`O usu[ario tem o e-mail ${user.email}`);
+  if (user.role) {
+    console.log(`A funcao do usuario e: ${user.role}`); // Se nao faz essa validacao aparece na tela undefined para o usuario, por ser um valor opcional
+  }
+}
+
+const u1: User = {
+  email: 'teste@teste.com',
+  role: 'Admin'
+}
+
+const u2: User = {
+  email: 'teste@teste.com',
+}
+
+showUserDetails(u1)
+showUserDetails(u2)
+
+// 3 - Propriedades readonly
+
+interface Car {
+  brand: string
+  readonly wheels: number // a palavra 'readonly' so permite que algo seja lido, nao pode ser alterado
+}
+
+const fusca: Car = {
+  brand: 'VW',
+  wheels: 4
+}
+
+console.log(fusca);
+
+// fusca.wheels = 5 -> aparce o seguinte erro 'Não é possível atribuir a 'wheels' porque é uma propriedade de somente leitura.'
+fusca.brand = 'Chevrolet' // aceita de boas
+
+//4 - Index signature -> Quando nao sabemos o nome das chaves, mas ja sabemos quais os tipos de um objeto ou array
+// da pra usar a palavra index ou key
+interface CoordObject {
+  [index: string]: number
+}
+
+let coords: CoordObject = {
+  x: 10
+}
+
+coords.y = 15
